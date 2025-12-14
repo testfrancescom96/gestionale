50844e6ffec9af215fe17f3834a5d7cf3000c8f0
Amministratore <admin@goontheroad.it>
2025-12-14 13:49:25 +0100

    Aggiornamento 14/12/2025 13:49:21,20

---
+++ AGGIORNA_SITO.bat
@@ -0,0 +1,34 @@
+@echo off
+title SUPER AGGIORNAMENTO AUTOMATICO (Passwordless)
+color 0A
+
+echo =======================================================
+echo      AGGIORNAMENTO TOTALE (GITHUB + SERVER)
+echo =======================================================
+echo.
+
+:: 1. GITHUB
+echo [Fase 1/2] Salvataggio e Invio su GitHub...
+git add .
+set msg=Aggiornamento %date% %time%
+set /p input="Scrivi un commento (o premi INVIO per andare veloce): "
+if not "%input%"=="" set msg=%input%
+git commit -m "%msg%"
+git push
+
+:: 2. SERVER
+echo.
+echo =======================================================
+echo [Fase 2/2] Comando al Server...
+echo.
+echo Mi collego al server per aggiornare il sito...
+echo (Se tutto e' configurato, non ti chiedera' nulla)
+echo =======================================================
+echo.
+ssh root@72.62.91.101 "./aggiorna.sh"
+
+echo.
+echo =======================================================
+echo  TUTTO FATTO! IL SITO E' AGGIORNATO E ONLINE.
+echo =======================================================
+pause
